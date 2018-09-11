8497ef9cb9a3f36079dee21cafbb5b218565af0f
Giovanni Schroevers <81179@glr.nl>
2018-09-11 11:48:06 +0200

    Slider met ES6

--- Public/Slideshow.js
+++ Public/Slideshow.js
@@ -1,37 +1,37 @@
 'use strict';
 
-var source = document.querySelector(".slide__img");
+const SOURCE = document.querySelector(".slide__img");
 var image = 1;
 var timer = true;
-var slideshow = window.setInterval(nextImage, 3000);
+var slideshow;
 
-function nextImage() {
+const nextImage = () => {
     if(image < 4 && image > 0) {
         image++;
     }else{
         image = 1;
     }
-    source.src = "img/thumb" + image + ".jpg";
+    SOURCE.src = `img/thumb${image}.jpg`;
     if(timer){
         window.clearInterval(slideshow);
         slideshow = window.setInterval(nextImage, 3000);
     }
-}
+};
 
-function previousImage() {
+const previousImage = () => {
     if(image > 1 && image < 5) {
         image--;
     }else{
         image = 4;
     }
-    source.src = "img/thumb" + image + ".jpg";
+    SOURCE.src = `img/thumb${image}.jpg`;
     if(timer){
         window.clearInterval(slideshow);
         slideshow = window.setInterval(nextImage, 3000);
     }
-}
+};
 
-function pause(){
+const pause = () =>{
     if(timer){
         window.clearInterval(slideshow);
         timer = false
@@ -40,12 +40,14 @@ function pause(){
         slideshow = window.setInterval(nextImage, 3000);
         timer = true;
     }
-}
+};
 
-document.addEventListener("keydown", function(event){
+document.addEventListener("keydown", event => {
     if(event.key === "ArrowRight"){
         nextImage();
     }else if(event.key === "ArrowLeft") {
         previousImage();
     }
 });
+
+slideshow = window.setInterval(nextImage, 3000);
